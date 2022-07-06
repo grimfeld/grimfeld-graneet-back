6d23a62ef10910d3895e5e852c968c3f6b1d8838
Grim's Desktop <paulperson.pro@gmail.com>
2022-07-06 11:48:15 +0200

    Added unit testing with jest

--- src/cities/cities.controller.spec.ts
+++ src/cities/cities.controller.spec.ts
@@ -1,3 +1,4 @@
+import { PrismaService } from './../prismaservice/prisma';
 import { Test, TestingModule } from '@nestjs/testing';
 import { CitiesController } from './cities.controller';
 import { CitiesService } from './cities.service';
@@ -8,7 +9,7 @@ describe('CitiesController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CitiesController],
-      providers: [CitiesService],
+      providers: [CitiesService, PrismaService],
     }).compile();
 
     controller = module.get<CitiesController>(CitiesController);
@@ -16,5 +17,26 @@ describe('CitiesController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  describe('cities endpoint', () => {
+    it('should return an array of 100 cities', async () => {
+      const result = await controller.findAll();
+      expect(result).toBeDefined();
+      expect(result).toHaveLength(100);
+    });
+  });
+
+  describe('filter endpoint', () => {
+    it('should find a specific city', async () => {
+      expect(await controller.filter('93800')).toStrictEqual([
+        {
+          codePostal: '93800',
+          codeCommune: '93031',
+          nomCommune: 'Épinay-sur-Seine',
+          libelleAcheminement: 'EPINAY SUR SEINE',
+        },
+      ]);
+    });
   });
 });
